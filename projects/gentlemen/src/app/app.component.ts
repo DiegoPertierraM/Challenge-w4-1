0653006c9e9dfe4e255dc2b69349f405e518e687
DiegoPertierraM <diegopmenendez@gmail.com>
2024-03-18 19:39:14 +0100

    Separate html into components and render cards

--- projects/gentlemen/src/app/app.component.ts
+++ projects/gentlemen/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InfoComponent } from './features/info/info.component';
+import { GentlemanComponent } from './features/gentleman/gentleman.component';
+import { gentlemen } from './features/gentleman/model/data.gentlemen';
 
 @Component({
   selector: 'isdi-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InfoComponent, GentlemanComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'gentlemen';
+  gentlemen = gentlemen;
 }
